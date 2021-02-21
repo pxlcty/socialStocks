19510261fbb75a688a2d3e79c9fe1e6af5fe557e
Michael McCracken <pxlcty@gmail.com>
2021-02-21 03:18:22 -0500

    Basic js search script works.

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -4,15 +4,16 @@ class StocksController < ApplicationController
         if params[:stock].present?
             # this looks up the Model def : 
             @stock = Stock.new_lookup(params[:stock])
-            #render json: @stock
             if @stock #check if @stock is not nil
-                render 'users/my_portfolio'
+                respond_to do |format|
+                  format.js { render partial: 'users/result' }
+                end
             else 
                 flash[:alert] = "Please enter a valid symbol to search"
                 redirect_to my_portfolio_path
             end
         else
-            flash[:alert] = "Please enter a valid symbol to search"
+            flash[:alert] = "Can't search empty text field"
             redirect_to my_portfolio_path
         end
     end
