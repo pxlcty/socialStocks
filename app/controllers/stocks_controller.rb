6844f95d147089ad6acf61c938933b11d35de6c6
Michael McCracken <pxlcty@gmail.com>
2021-02-21 02:44:25 -0500

    Empty & non existing ticker_symbols error handling

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,9 +1,19 @@
 class StocksController < ApplicationController
 
     def search
-        @stock = Stock.new_lookup(params[:stock])
-        #render json: @stock
-        render 'users/my_portfolio'
+        if params[:stock].present?
+            # this looks up the Model def : 
+            @stock = Stock.new_lookup(params[:stock])
+            #render json: @stock
+            if @stock #check if @stock is not nil
+                render 'users/my_portfolio'
+            else 
+                flash[:alert] = "Please enter a valid symbol to search"
+                redirect_to my_portfolio_path
+            end
+        else
+            flash[:alert] = "Please enter a valid symbol to search"
+            redirect_to my_portfolio_path
+        end
     end
-
 end
