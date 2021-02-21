dfe2b60baab44596727577a9cb77d2e9e14381aa
Michael McCracken <pxlcty@gmail.com>
2021-02-21 13:53:34 -0500

    Frontend display stocks following & backend add new stocks or use existing row of stocks table

--- app/controllers/user_stocks_controller.rb
+++ app/controllers/user_stocks_controller.rb
@@ -1,2 +1,14 @@
 class UserStocksController < ApplicationController
+
+    def create
+        stock = Stock.check_db(params[:ticker]) # does ticker exist within current table?
+        if stock.blank? # if stocks does not currently exist in table
+            stock = Stock.new_lookup(params[:ticker]) 
+            stock.save
+        end
+        @user_stock = UserStock.create(user: current_user, stock: stock)
+        flash[:notice] = "Stock #{stock.name} was successfully added to your portfolio"
+        redirect_to my_portfolio_path
+    end
+
 end
